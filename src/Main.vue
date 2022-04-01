a65a5b24cc30697a958fda2d330d2234d919c8f6
Dustella <fdnoaivj@outlook.com>
2022-04-01 14:16:45 +0800

    jb的shelf不会玩

--- src/Main.vue
+++ src/Main.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <main-footer />
-    <router-view />
+    <main-footer/>
+    <router-view/>
   </div>
 
 </template>
 
 <script lang="ts" setup>
 import MainFooter from "./components/Base/Footer.vue"
+import {loginState} from "./store/loginStatus";
+import {onMounted} from "vue";
+import {userInfo} from "./store/userInfo";
+// begin init login state
+const login = loginState()
+const info = userInfo();
+
+onMounted(async () => {
+      login.loadfromLocal();
+      await info.fetchInfo(login.userid);
+    }
+)
+
 </script>
